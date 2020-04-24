1c5947c758dba58f4417dd66f1c3a4772c0ec4f5
Ty <tptorres5@gmail.com>
2020-04-24 10:10:04 -0400

    Initial Commit

---
+++ Skid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skid
+//
+//  Created by Tyler Torres on 4/24/20.
+//  Copyright © 2020 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
